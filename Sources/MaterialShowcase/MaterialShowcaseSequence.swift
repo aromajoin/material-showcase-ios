3d37ec442fe5299d9855b4064b019ca63ccba26c
DungPham <dung.pham@alo.vn>
2022-05-17 15:06:01 +0700

    Add showShadow param for start sequence

--- Sources/MaterialShowcase/MaterialShowcaseSequence.swift
+++ Sources/MaterialShowcase/MaterialShowcaseSequence.swift
@@ -9,59 +9,59 @@
 import Foundation
 
 public class MaterialShowcaseSequence {
-  
-  var showcaseArray : [MaterialShowcase] = []
-  var currentCase : Int = 0
-  var key : String?
-  
-  public init() { }
-  
-  public func temp(_ showcase: MaterialShowcase) -> MaterialShowcaseSequence {
-    showcaseArray.append(showcase)
-    return self
-  }
-  public func start() {
-    guard !getUserState(key: self.key) else {
-      return
+    var showShadow: Bool = false
+    var showcaseArray : [MaterialShowcase] = []
+    var currentCase : Int = 0
+    var key : String?
+    
+    public init() { }
+    
+    public func temp(_ showcase: MaterialShowcase) -> MaterialShowcaseSequence {
+        showcaseArray.append(showcase)
+        return self
     }
-    showcaseArray.first?.show(completion: increase)
-  }
-  func increase() -> Void {
-    self.currentCase += 1
-  }
-  
-  /// Set user show retry
-  public func setKey(key : String? = nil) -> MaterialShowcaseSequence {
-    guard key != nil else {
-      return self
+    public func start() {
+        guard !getUserState(key: self.key) else {
+            return
+        }
+        showcaseArray.first?.show(hasShadow: showShadow, completion: increase)
     }
-    self.key = key
-    return self
-  }
-  
-  /// Remove user state
-  public func removeUserState(key : String = MaterialKey._default.rawValue) {
-    UserDefaults.standard.removeObject(forKey: key)
-  }
-  /// Remove user state
-  func getUserState(key : String?) -> Bool {
-    guard key != nil else {
-      return false
+    func increase() -> Void {
+        self.currentCase += 1
     }
-    return UserDefaults.standard.bool(forKey: key!)
-  }
-  
-  public func showCaseWillDismis() {
-    guard self.showcaseArray.count > currentCase else {
-      //last index
-      guard self.key != nil else {
-        return
-      }
-      UserDefaults.standard.set(true, forKey: key!)
-      return
+    
+    /// Set user show retry
+    public func setKey(key : String? = nil) -> MaterialShowcaseSequence {
+        guard key != nil else {
+            return self
+        }
+        self.key = key
+        return self
     }
-    showcaseArray[currentCase].show(completion: self.increase)
-  }
-  
+    
+    /// Remove user state
+    public func removeUserState(key : String = MaterialKey._default.rawValue) {
+        UserDefaults.standard.removeObject(forKey: key)
+    }
+    /// Remove user state
+    func getUserState(key : String?) -> Bool {
+        guard key != nil else {
+            return false
+        }
+        return UserDefaults.standard.bool(forKey: key!)
+    }
+    
+    public func showCaseWillDismis() {
+        guard self.showcaseArray.count > currentCase else {
+            //last index
+            guard self.key != nil else {
+                return
+            }
+            UserDefaults.standard.set(true, forKey: key!)
+            return
+        }
+        showcaseArray[currentCase].show(hasShadow: showShadow, completion: self.increase)
+    }
+    
 }
 
